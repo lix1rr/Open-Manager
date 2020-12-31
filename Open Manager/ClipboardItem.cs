e1d8b5d5f5e4b2711e4046da20b6e5b3d0d42dfa
lix1rr <>
2020-12-31 10:18:26 +0800

    Initial commit

---
+++ Open Manager/ClipboardItem.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Open_Manager
+{
+	class ClipboardItem
+	{
+		public IDataObject dataObject;
+		public string typeString;
+		enum ViewingType { 
+			Image,
+			Text
+		};
+		ViewingType type;
+		object data;
+		//Bitmap when type is Image, string when type is text
+		struct Pair {
+			public string dataFormat;
+			public string name;
+			public Pair(string dataFormat, string name)
+			{
+				this.dataFormat = dataFormat;
+				this.name = name;
+			}
+		}
+
+		// Maps data format strings to more readable MIME like type names, ones at the top have more priority
+		static Pair[] order =  {
+			new Pair( DataFormats.PenData,             "others/pen data"),
+			new Pair( DataFormats.Bitmap,              "image/bitmap" ),
+			new Pair( DataFormats.Tiff,                "image/tiff"),
+			new Pair( DataFormats.Dib,                 "image/device independent bitmap"),
+			new Pair( DataFormats.CommaSeparatedValue, "text/csv"),
+			new Pair( DataFormats.Html,                "text/html"),
+			new Pair( DataFormats.Rtf,                 "text/rtf"),
+			new Pair( DataFormats.Xaml,                "text/xaml"),
+			new Pair( DataFormats.Riff,                "audio/riff"),
+			new Pair( DataFormats.WaveAudio,           "audio/wav"),
+			new Pair( DataFormats.FileDrop,            "others/file drop"),
+			new Pair( DataFormats.MetafilePicture,     "others/meta file picture"),
+			new Pair( DataFormats.Palette,             "others/palette"),
+			new Pair( DataFormats.Serializable,        "others/persistant object"),
+			new Pair( DataFormats.SymbolicLink,        "others/symbolic link"),
+			new Pair( DataFormats.XamlPackage,         "others/xaml package"),
+			new Pair( DataFormats.Text,                "text"),
+			new Pair( DataFormats.UnicodeText,         "text/unciode"),
+			new Pair( DataFormats.OemText,             "text/oem"),
+			new Pair( DataFormats.Locale,              "others/locale"),
+			new Pair( DataFormats.Dif,                 "others/data interchange format"),
+			new Pair( DataFormats.EnhancedMetafile,    "others/enhanced metafile"),
+			new Pair( DataFormats.StringFormat,        "text/system string")
+		};
+
+		
+		// byte[] when type is Binary
+		// Bitmap when type is Image
+		// string when type is String
+		public ClipboardItem(IDataObject dataObject)
+		{
+			this.dataObject = dataObject;
+			if ((data = dataObject.GetData(DataFormats.Bitmap)) != null)
+			{
+				type = ViewingType.Image;
+				
+			}
+			else if ((data = dataObject.GetData(DataFormats.StringFormat)) != null)
+			{
+				type = ViewingType.Text;
+			}
+			else
+			{
+				throw new Exception("cannot make clipboard item");
+			}
+			foreach (var item in order) {
+				if (dataObject.GetFormats().Contains(item.dataFormat))
+				{
+					typeString = item.name;
+					return;
+				}
+			}
+			typeString = "unknown/" + dataObject.GetFormats()[0];
+			
+			throw new Exception("cannot make clipboard item");
+		}
+
+		public Grid MakeControl()
+		{
+			
+			Console.WriteLine("! Copy");
+			var grid = new Grid();
+			grid.Margin = new Thickness(0, 0, 0, 10);
+
+			var border = new Border();
+			border.BorderBrush = new SolidColorBrush(Color.FromArgb(0xFF, 0xAA, 0xAA, 0xAA));
+			border.BorderThickness = new Thickness(1);
+			border.CornerRadius = new CornerRadius(5);
+			grid.Children.Add(border);
+
+			var stackPanel = new StackPanel();
+			stackPanel.Margin = new Thickness(10);
+			grid.Children.Add(stackPanel);
+
+			var info = new TextBlock();
+			info.TextWrapping = TextWrapping.Wrap;
+			info.Inlines.Add(new Italic(new Run("type="+typeString)));
+			stackPanel.Children.Add(info);
+
+			if (type == ViewingType.Text)
+			{
+				var text = new TextBlock();
+				text.FontFamily = new FontFamily("Consolas");
+				text.TextWrapping = TextWrapping.Wrap;
+				text.Text = (string)data;
+				stackPanel.Children.Add(text);
+			}
+			else if (type == ViewingType.Image)
+			{
+				var image = new Image();
+				image.Source = (InteropBitmap)data;
+				stackPanel.Children.Add(image);
+			}
+
+			return grid;
+			/*
+			 * 
+			 <Grid>
+				<Border BorderBrush="#FFAAAAAA" BorderThickness="1" CornerRadius="5"/>
+				<StackPanel Margin="10">
+					<TextBlock>
+						<Italic>type="text":</Italic>
+					</TextBlock>
+					<TextBlock FontFamily="Consolas" TextWrapping="Wrap">I am a crippled dog with a spoon</TextBlock>
+					or
+					<Image Source="http://i.imgur.com/aIf7B0P.jpg" />
+				</StackPanel>		
+			</Grid>
+			*/
+		}
+	}
+}
---
+++ Open Manager/MainWindow.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Open_Manager
+{
+
+
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		static Dictionary<IntPtr, MainWindow> hwndsAndWindow = new Dictionary<IntPtr, MainWindow>();
+		List<ClipboardItem> clipboardItems = new List<ClipboardItem>();
+
+		[DllImport("User32.dll", CharSet = CharSet.Auto)]
+		public static extern IntPtr SetClipboardViewer(IntPtr hWndNewViewer);
+		public MainWindow()
+		{
+			InitializeComponent();
+			MouseDown += Window_MouseDown;
+			Loaded += Window_Loaded;
+
+		}
+		
+		private void Window_Loaded(object sender, RoutedEventArgs e)
+		{
+			Console.WriteLine("register hook");
+			Loaded -= Window_Loaded;
+			IntPtr hwnd = new WindowInteropHelper(this).Handle;
+			hwndsAndWindow.Add(hwnd, this);
+			HwndSource source = HwndSource.FromHwnd(hwnd);
+			source.AddHook(new HwndSourceHook(Window_WndProc));
+			SetClipboardViewer(hwnd);
+		}
+		private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+		{
+			if (e.ChangedButton == MouseButton.Left)
+				DragMove();
+		}
+		
+		private void OnCopy()
+		{
+			var dataObject = Clipboard.GetDataObject();
+			if (dataObject == null || (clipboardItems.Count > 0 && Clipboard.IsCurrent(clipboardItems[0].dataObject)))
+			{
+				return;
+			}
+			ClipboardItem item = new ClipboardItem(dataObject);
+			clipboardItems.Add(item);
+			var control = item.MakeControl();
+			if (control != null)
+			{
+				
+				itemList.Children.Insert(0, control);
+			}
+			
+		}
+		const int WM_DRAWCLIPBOARD = 0x0308;
+		private static IntPtr Window_WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+		{
+			if (msg == WM_DRAWCLIPBOARD)
+			{
+				hwndsAndWindow[hwnd].OnCopy();
+			}
+			return IntPtr.Zero;
+		}
+		
+	}
+}
+/*
+ * 
+ * <<!--
+	
+	
+	
+	<Grid>
+	<ScrollViewer Margin="0,10,0,0" VerticalScrollBarVisibility="Visible" Height="1000">
+		<StackPanel>
+			<TextBlock Height="10000">
+				as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>as\n<LineBreak/>
+			</TextBlock>
+		</StackPanel>
+
+	</ScrollViewer>
+	</Grid>
+	
+-->
+*/
